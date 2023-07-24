7be301a2e16f388c38b2b0f68d7000b4d15f2da9
Jeiel <github.esvlc@slmail.me>
2023-07-24 09:59:56 -0300

    Update bES.sh

--- Language/bES.sh
+++ Language/bES.sh
@@ -1,16 +1,13 @@
-Claro, aqui está o script com todas as ocorrências de `echo "texto"` substituídas pelo texto em espanhol:
-
-```
 #!/bin/bash
 
 # ¿Te gustaría hacer una donación a través de pix ?!
 #
-# Cualquier monto: https://iti.itau/receber-pix/?chargeId=9bdcfe03-f432-473c-9487-782f3f3f6b9b
+# Cualquier cantidad es bienvenida: https://iti.itau/receber-pix/?chargeId=9bdcfe03-f432-473c-9487-782f3f3f6b9b
 #
-# Nota: El proyecto no está afiliado a Square Cloud directa o indirectamente.
+# Nota: El proyecto no está afiliado directa o indirectamente a Square Cloud.
 
 exibir_menu() {
-  cubo="
+    cubo="
   ______                                       _______  _                     _ 
  / _____)                                     (_______)| |                   | |
 ( (____    ____  _   _  _____   ____  _____    _       | |   ___   _   _   __| |
@@ -19,75 +16,105 @@ exibir_menu() {
 (______/  \__  ||____/ \_____||_|    |_____)   \______) \_) \___/ |____/  \____|
              |_|                                                                
 "
-  echo "$cubo" | lolcat
-  echo "1. Verificar el estado de la aplicación" | lolcat
-  echo "2. Verificar los registros de la aplicación" | lolcat
-  echo "3. Acceder a la copia de seguridad de la aplicación" | lolcat
-  echo "4. Agregar otra aplicación" | lolcat
-  echo "5. Salir" | lolcat
+    echo "$cubo" | lolcat
+    echo "1. Verificar estado de la aplicación" | lolcat
+    echo "2. Verificar registros de la aplicación" | lolcat
+    echo "3. Acceder a la copia de seguridad de la aplicación" | lolcat
+    echo "4. Añadir otra aplicación" | lolcat
+    echo "5. Salir" | lolcat
 }
 
 ler_escolha() {
-  read -p "Ingrese una opción: " escolha
+    read -p "Ingrese una opción: " choice
 }
 
 ler() {
-  read -p "Ingrese el valor del ID: " ID
-  read -p "Ingrese el valor de AUTORIZACIÓN: " AUTORIZACIÓN
+    read -p "Ingrese el valor de APIKey: " APIKey
+    read -p "Ingrese el valor de AUTHORIZATION: " AUTHORIZATION
 
-  # Guardar en el archivo CONFIG.SH
-  echo "export ID=\"$ID\"" >> "$CONFIG_FILE"
-  echo "export AUTORIZATION=\"$AUTHORIZATION\"" >> "$CONFIG_FILE"
+    # Guardar en el archivo CONFIG.SH
+    echo "export APIKey=\"$APIKey\"" >> "$CONFIG_FILE"
+    echo "export AUTHORIZATION=\"$AUTHORIZATION\"" >> "$CONFIG_FILE"
 }
 
 carrega() {
-  # Carga los datos registrados en el archivo creado
-  if [ -f "$CONFIG_FILE" ]; then
-    source "$CONFIG_FILE"
-  fi
+    # Cargar los datos guardados del archivo creado
+    if [ -f "$CONFIG_FILE" ]; then
+        source "$CONFIG_FILE"
+    fi
 }
 
 adicionar_outra_aplicacao() {
-  read -p "¿Desea reemplazar esta aplicación? (S/N): " resposta
-  if [[ $resposta == "S" || $resposta == "s" ]]; then
-    ler
-    adicionar_outra_aplicacao
-  fi
+    read -p "¿Desea reemplazar esta aplicación? (S/N): " respuesta
+    if [[ $respuesta == "S" || $respuesta == "s" ]]; then
+        ler
+        adicionar_outra_aplicacao
+    fi
 }
 
 executar_opcao() {
-  case $escolha in
-    1)
-      echo "Verificando el estado de la aplicación..." | lolcat
-      curl -X GET "https://api.squarecloud.app/v2/apps/$ID/status" \
-        -H "Authorization: $AUTHORIZATION" \
-        | jq
-      sleep 3
-      ;;
-    2)
-      echo "Verificando los registros de la aplicación..." | lolcat
-      curl -X GET "https://api.squarecloud.app/v2/apps/$ID/logs" \
-        -H "Authorization: $AUTHORIZATION" \
-        | jq
-      sleep 5
-      ;;
-    3)
-      echo "Accediendo a la copia de seguridad de la aplicación..." | lolcat
-      json=$(curl -X GET "https://api.squarecloud.app/v2/apps/$ID/backup" \
-        -H "Authorization: $AUTHORIZATION" \
-        | jq)
-      downloadURL=$(echo "$json" | jq -r '.response.downloadURL')
-      if [[ $downloadURL == "null" ]]; then
-        echo "Error."
-      else
-        echo "Abriendo la URL de descarga en el navegador..." | lolcat
-        if xdg-open "$downloadURL" > /dev/null 2>&1; then
-          echo "¡Hecho!"
-        else
-          echo "No se encontró ningún navegador predeterminado para abrir la URL." | lolcat
-        fi
-      fi
-      ;;
-    4)
-      echo "Agregando otra aplicación..." | lolcat
-      ler
+    case $choice in
+        1)
+            echo "Verificando estado de la aplicación..." | lolcat
+            curl -k GET "https://api.squarecloud.app/v2/apps/$APIKey/status" \
+                -H "Authorization: $AUTHORIZATION" \
+                | jq
+            sleep 3
+            ;;
+        2)
+            echo "Verificando registros de la aplicación..." | lolcat
+            curl -k GET "https://api.squarecloud.app/v2/apps/$APIKey/logs" \
+                -H "Authorization: $AUTHORIZATION" \
+                | jq
+            sleep 5
+            ;;
+        3)
+            echo "Accediendo a la copia de seguridad de la aplicación..." | lolcat
+            json=$(curl -k GET "https://api.squarecloud.app/v2/apps/$APIKey/backup" \
+                -H "Authorization: $AUTHORIZATION" \
+                | jq)
+            downloadURL=$(echo "$json" | jq -r '.response.downloadURL')
+            if [[ $downloadURL == "null" ]]; then
+                echo "Error."
+            else
+                echo "Abriendo la URL de descarga en el navegador..." | lolcat
+                if xdg-open "$downloadURL" > /dev/null 2>&1; then
+                    echo "¡Hecho!"
+                else
+                    echo "No se encontró un navegador predeterminado para abrir la URL." | lolcat
+                fi
+            fi
+            ;;
+        4)
+            echo "Añadiendo otra aplicación..." | lolcat
+            ler
+            adicionar_outra_aplicacao
+            ;;
+        5)
+            echo "Saliendo..." | lolcat
+            exit
+            ;;
+        *)
+            echo "Opción inválida. Por favor, intente nuevamente." | lolcat
+            ;;
+    esac
+}
+
+# Configuración del archivo
+CONFIG_FILE="config.sh"
+
+# Cargar los datos guardados
+carrega
+
+# Verificar los datos guardados
+if [ -z "$APIKey" ] || [ -z "$AUTHORIZATION" ]; then
+    echo "Las credenciales de entorno APIKey y AUTHORIZATION no están definidas."
+    ler
+    adicionar_outra_aplicacao
+fi
+
+while true; do
+    exibir_menu
+    ler_escolha
+    executar_opcao
+done
